989b95ad14fd9e7b57caf64a1249631280451ee8
Arthur Battut <arthur@soixantecircuits.fr>
2014-11-03 01:23:56 +0100

    save contact message in database

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -7,10 +7,20 @@
  * # ContactCtrl
  * Controller of the colorApp
  */
-angular.module('colorApp').controller('ContactCtrl', ['$rootScope','$scope', 
-    function ($rootScope, $scope) {
+angular.module('colorApp').controller('ContactCtrl', ['$rootScope','$scope', '$firebase',
+    function ($rootScope, $scope, $firebase) {
         $rootScope.counterLoaded = true;
-        console.log($rootScope.counterLoaded);
+
+        var ref = new Firebase('https://'+fireName+'.firebaseio.com/messages');
+        $scope.sendMessage = function(message){
+            var item = {
+                'date': new Date(),
+                'from': message.sender,
+                'mail': message.mail,
+                'message': message.message
+            }
+            ref.push(item);
+            $scope.message = {};
+        }
     }
-    
 ]);
